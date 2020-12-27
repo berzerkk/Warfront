e5fc3180e5f34435faa9efe9859b60203d45007f
Ghislain98 <g.gloria1998@gmail.com>
2020-12-27 02:18:02 +0100

    Inventory, ressources, scrolling

---
+++ Assets/Scripts/UI/InventoryController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    private bool activated = false;
+    //public GameObject[] weapons;
+    //public GameObject CurrentWeapon;
+    public float inventoryTime = 0;
+    //public int weaponNumber = 0;
+    public Canvas canvas;
+
+
+    private void Start()
+    {
+        //CurrentWeapon = weapons[0];
+    }
+
+    private void Update()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") != 0){
+            activated = true;
+            inventoryTime = 2f;
+            if(Input.GetAxis("Mouse ScrollWheel") > 0){
+                //weaponNumber = (weaponNumber + 1);
+            }
+            if(Input.GetAxis("Mouse ScrollWheel") < 0){
+                //weaponNumber = (weaponNumber - 1);
+            }
+        }
+        else if(inventoryTime < 0f)
+        {
+            activated = false;
+        }
+        
+        //CurrentWeapon = weapons[weaponNumber];
+        if (inventoryTime >= -1f)
+        {
+            inventoryTime = inventoryTime - Time.deltaTime;
+        }
+        canvas.enabled = activated;
+    }
+}
